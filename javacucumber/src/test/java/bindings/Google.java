08ef95f9292f2ddf9435616194cc67b2ca12bc3e
zr123 <zr123>
2019-01-28 21:36:58 +0100

    fixed "mvn test"

--- javacucumber/src/test/java/bindings/Google.java
+++ javacucumber/src/test/java/bindings/Google.java
@@ -1,27 +1,23 @@
 package bindings;
 
-import cucumber.api.PendingException;
-import cucumber.api.java.en.*;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.assertTrue;
 
 public class Google {
-    WebDriver driver;
-
+    private WebDriver driver;
 
     @Given("^that i am on \"([^\"]*)\"$")
     public void thatIAmOn(String arg0) throws Throwable {
         System.setProperty("webdriver.chrome.driver", "C:\\Daten\\driver\\chromedriver.exe");
-       driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.get(arg0);
         driver.manage().window().maximize();
