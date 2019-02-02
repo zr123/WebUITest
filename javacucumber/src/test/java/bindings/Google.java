1e2d3708aecb111b11f4082305ff7499c86c63f1
zr123 <zr123>
2019-02-03 00:08:20 +0100

    cleanup and more travis config

--- javacucumber/src/test/java/bindings/Google.java
+++ javacucumber/src/test/java/bindings/Google.java
@@ -1,5 +1,7 @@
 package bindings;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -7,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,10 +17,19 @@ import java.util.concurrent.TimeUnit;
 public class Google {
     private WebDriver driver;
 
-    @Given("^that i am on \"([^\"]*)\"$")
-    public void thatIAmOn(String arg0) throws Throwable {
+    @Before
+    public void setup() throws Exception{
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    @Given("^that i am on \"([^\"]*)\"$")
+    public void thatIAmOn(String arg0) throws Throwable {
         driver.get(arg0);
         driver.manage().window().maximize();
     }
@@ -33,6 +45,5 @@ public class Google {
     public void theFirstResultWillBeTheWebsiteOfBundesbank() throws Throwable {
         driver.findElement(By.xpath("//h3[text()='Deutsche Bundesbank: Startseite']")).click();
         Thread.sleep(1000);
-        driver.quit();
     }
 }
