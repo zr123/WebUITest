20e9c9a72275d65ed157b3a1645961a14b8fd483
zr123 <25320035+zr123@users.noreply.github.com>
2019-02-15 14:52:13 +0100

    Internetexplorer -> Chrome

--- javaselenium/src/test/java/TestGUI.java
+++ javaselenium/src/test/java/TestGUI.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
-import org.openqa.selenium.*;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,7 +12,7 @@ public class TestGUI {
 
     @Test
     public void testMinimalWikipedia(){
-        WebDriver driver = new InternetExplorerDriver();
+        WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://google.com/");
         WebElement inputQuery = driver.findElement(By.name("q"));
