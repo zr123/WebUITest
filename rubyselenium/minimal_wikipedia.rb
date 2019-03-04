0d6e27ab3272545a455b8c0584236016834e0bbf
zr123 <25320035+zr123@users.noreply.github.com>
2019-03-04 11:22:08 +0100

    Improved Ruby/Selenium test

--- rubyselenium/minimal_wikipedia.rb
+++ rubyselenium/minimal_wikipedia.rb
@@ -1,9 +1,18 @@
 require 'selenium-webdriver'
-
-driver = Selenium::WebDriver.for :chrome
-
-driver.get('https://de.wikipedia.org')
-#sleep 1
-driver.find_element(link_text: "Zufälliger Artikel").click 
-sleep 5
-driver.close
+require "test/unit"
+ 
+class TestGUI < Test::Unit::TestCase
+ 
+  def testMinimalWikipedia
+    driver = Selenium::WebDriver.for :chrome
+    driver.get('https://google.com')
+    driver.manage.timeouts.implicit_wait = 5
+    element = driver.find_element(name: 'q')
+    element.send_keys("Wikipedia")
+    element.submit
+    driver.find_element(xpath: "//h3[text()='Wikipedia']").click
+    assert_equal("Wikipedia", driver.title)
+    driver.quit
+  end
+ 
+end
